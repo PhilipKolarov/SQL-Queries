7ba738b171b0043bf5144f3226a4ae8d0d5301b1
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-09-18 19:53:38 +0300

    Create crud-exercises.sql

---
+++ crud-exercises.sql
@@ -0,0 +1,12 @@
+SELECT * FROM Departments;
+
+SELECT Name FROM Departments;
+
+SELECT FirstName, LastName, Salary
+FROM Employees;
+
+SELECT FirstName, MiddleName, LastName
+FROM Employees;
+
+SELECT FirstName + '.' + LastName + '@softuni.bg' AS [Full Email Address]
+FROM Employees;
