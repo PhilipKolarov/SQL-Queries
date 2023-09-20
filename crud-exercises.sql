7e5dd3fb979852f517ad8356a266addd87f4852f
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-09-20 20:43:51 +0300

    Update crud-exercises.sql
    Add 5 more select queries

--- crud-exercises.sql
+++ crud-exercises.sql
@@ -25,3 +25,24 @@ WHERE Salary BETWEEN 20000 AND 30000;
 SELECT CONCAT_WS(' ', FirstName, MiddleName, LastName)
 FROM Employees
 WHERE Salary IN(25000, 14000, 12500, 23600);
+
+SELECT FirstName, LastName
+FROM Employees
+WHERE ManagerID IS NULL;
+
+SELECT FirstName, LastName, Salary
+FROM Employees
+WHERE Salary > 50000
+ORDER BY Salary DESC;
+
+SELECT TOP(5)FirstName, LastName
+FROM Employees
+ORDER BY Salary DESC;
+
+SELECT FirstName, LastName
+FROM Employees
+WHERE DepartmentID <> 4;
+
+SELECT *
+FROM Employees
+ORDER BY Salary DESC, FirstName, LastName DESC, MiddleName;
