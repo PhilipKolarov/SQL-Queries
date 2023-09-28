49dd78d348cb42ab8344e73da9eebc9086c8fa18
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-09-28 09:19:59 +0300

    Update table-relations-exercises.sql

--- table-relations-exercises.sql
+++ table-relations-exercises.sql
@@ -100,3 +100,35 @@ VALUES (101, 'Johnny', NULL),
 	   (104, 'David', 101),
 	   (105, 'Herbert', NULL),
 	   (106, 'Sam', 105);
+
+
+
+CREATE TABLE Majors(
+	MajorID INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE Subjects(
+	SubjectID INT PRIMARY KEY IDENTITY,
+	SubjectName VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE Students(
+	StudentID INT PRIMARY KEY,
+	StudentNumber INT UNIQUE NOT NULL,
+	StudentName NVARCHAR(50) NOT NULL,
+	MajorID INT FOREIGN KEY REFERENCES Majors(MajorID) NOT NULL
+);
+
+CREATE TABLE Agenda(
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID),
+	SubjectID INT FOREIGN KEY REFERENCES Subjects(SubjectID),
+	PRIMARY KEY(StudentID, SubjectID)
+);
+
+CREATE TABLE Payments(
+	PaymentID INT PRIMARY KEY,
+	PaymentDate DATETIME2,
+	PaymentAmount DECIMAL(8,2),
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID)
+);
