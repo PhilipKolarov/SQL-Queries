6675c8bebfcfb40cba61cb08bdcb6e7f18d120a6
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-10-21 14:10:48 +0300

    Create tourists-booked-in-hotels.sql

---
+++ Exam 15-10-23/tourists-booked-in-hotels.sql
@@ -0,0 +1,8 @@
+-- 09 Tourists booked in Hotels
+SELECT h.[Name] AS HotelName, r.Price AS RoomPrice
+FROM Tourists AS t
+JOIN Bookings AS b ON b.TouristId = t.Id
+JOIN Hotels AS h ON h.Id = b.HotelId
+JOIN Rooms AS r ON r.Id = b.RoomId
+WHERE t.[Name] NOT LIKE '%ez'
+ORDER BY r.Price DESC
